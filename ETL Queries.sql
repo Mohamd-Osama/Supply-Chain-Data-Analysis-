6aadbdd90d1fd7dce9fc81ac33b7bcfcf0fd7bb4
Mohamed Osama <mo0946277@outlook.com>
2025-11-24 20:18:35 +0200

    Add files via upload
    Initial upload of Supply Chain Analysis Project

---
+++ ETL Queries.sql
@@ -0,0 +1,81 @@
+--                                Source Queries For ETL 
+
+--Customers Analysis
+select 
+C.Customer,
+C.[Primary Contact],
+D.[Calendar Year],
+S.Profit,
+S.Quantity,
+S.[Sale Key],
+S.[Customer Key]
+from Fact.Sale S
+join Dimension.Customer C on S.[Customer Key]=C.[Customer Key]
+join Dimension.Date D on S.[Invoice Date Key]=D.Date
+
+--Cities Sales Analysis 
+select 
+C.[City Key],
+C.City,
+C.[Sales Territory],
+S.Quantity,
+S.Profit,
+Cust.[Customer Key],
+D.[Calendar Year]
+from Fact.Sale S
+join Dimension.City C on S.[City Key]=C.[City Key]
+join Dimension.Customer Cust on S.[Customer Key]=Cust.[Customer Key]
+join Dimension.Date D on S.[Invoice Date Key]=D.Date
+
+--Supplier 
+select
+S.Supplier,
+S.[Supplier Key],
+D.Month,
+D.[Calendar Year],
+P.[Ordered Quantity],
+St.[Stock Item]
+from Fact.Purchase P
+join Dimension.Date D on P.[Date Key]=D.Date
+join Dimension.Supplier S on P.[Supplier Key]=S.[Supplier Key]
+join Dimension.[Stock Item] St on P.[Stock Item Key]=St.[Stock Item Key]
+
+
+--Trend Overtimes
+select 
+D.[Calendar Year],
+D.Month,
+S.Profit,
+S.Quantity
+from Fact.Sale S
+join Dimension.[Date] D on S.[Invoice Date Key]=D.Date
+------------------------------------------------
+-- sales person
+select
+s.[Sale key],
+s.[Salesperson key],
+s.Quantity,
+s.Profit,
+e.Employee,
+D.[Calendar Year],
+C.[Customer Key]
+from Fact.Sale s
+join Dimension.Employee e on S.[Salesperson Key]=E.[Employee Key]
+join Dimension.Customer C on S.[Customer Key]=C.[Customer Key]
+join Dimension.Date D on S.[Invoice Date Key]=D.Date
+------------------------------------------------
+--total Orders for each year in each month
+select St.[Stock Item Key],
+St.[Stock Item],
+D.[Calendar Month Number],
+D.[Calendar Year],
+count(O.[Order Date Key]) as total_orders
+from Fact.[Order] O
+join Dimension.[Stock Item] St on O.[Stock Item Key]=St.[Stock Item Key]
+join Dimension.Date D on O.[Order Date Key]=D.Date
+group by St.[Stock Item Key],
+St.[Stock Item],D.[Calendar Year],
+D.[Calendar Month Number]
+order by [Calendar Year] desc
+------------------------------------------------
+
---
+++ SQL Scripit.sql
@@ -0,0 +1,111 @@
+﻿use WideWorldImportersDW
+--Q1: ما إجمالي المبيعات (بالقيمة) لكل عميل خلال السنة؟
+SELECT 
+    c.Customer,
+    SUM(s.Quantity * s.[Unit Price]) AS TotalSalesRevenue
+FROM Fact.Sale s
+JOIN Dimension.Customer c ON s.[Customer Key] = c.[Customer Key]
+GROUP BY c.Customer
+ORDER BY TotalSalesRevenue DESC;
+
+--ما أكثر 10 عملاء شراءً (حسب الكمية)؟
+
+SELECT TOP 10
+    c.Customer,
+    SUM(s.Quantity) AS TotalQuantitySold
+FROM Fact.Sale s
+JOIN Dimension.Customer c ON s.[Customer Key] = c.[Customer Key]
+JOIN Dimension.Date d ON s.[Invoice Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY c.Customer
+ORDER BY TotalQuantitySold DESC;
+
+--ما إجمالي المبيعات حسب المدينة؟
+
+SELECT 
+    ci.City,
+    SUM(s.[Total Excluding Tax] + s.[Tax Amount]) AS TotalSalesRevenue
+FROM Fact.Sale s
+JOIN Dimension.Customer c ON s.[Customer Key] = c.[Customer Key]
+JOIN Dimension.City ci ON c.[Lineage Key] = ci.[City Key]
+JOIN Dimension.Date d ON s.[Invoice Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY ci.City
+ORDER BY TotalSalesRevenue DESC;
+
+
+
+
+-- ما نسبة التسليم في الوقت (On-time Delivery Rate) لكل مورد؟
+SELECT 
+    sup.Supplier,
+    COUNT(*) AS TotalPurchases,
+    SUM(CASE WHEN p.[Date Key] <= p.[Date Key] THEN 1 ELSE 0 END) AS OnTimeDeliveries,
+    ROUND(
+        SUM(CASE WHEN p.[Date Key] <= p.[Date Key] THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2
+    ) AS OnTimeDeliveryRate_Pct
+FROM Fact.Purchase p
+JOIN Dimension.Supplier sup ON p.[Supplier Key] = sup.[Supplier Key]
+JOIN Dimension.Date d ON p.[Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY sup.Supplier
+ORDER BY OnTimeDeliveryRate_Pct DESC;
+
+--ما عدد الطلبات التي تم معالجتها لكل موظف؟
+SELECT 
+    e.[Employee],
+    COUNT(s.[Sale Key]) AS TotalOrdersProcessed
+FROM Fact.Sale s
+JOIN Dimension.Employee e ON s.[Salesperson Key] = e.[Employee Key]
+JOIN Dimension.Date d ON s.[Delivery Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY e.[Employee]
+ORDER BY TotalOrdersProcessed DESC;
+
+
+--ما إجمالي قيمة المبيعات التي حققها كل موظف؟
+SELECT 
+    e.[Employee],
+    SUM(s.[Total Including Tax] + s.[Tax Amount]) AS TotalSalesValue
+FROM Fact.Sale s
+JOIN Dimension.Employee e ON s.[Salesperson Key] = e.[Employee Key]
+JOIN Dimension.Date d ON s.[Delivery Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY e.[Employee]
+ORDER BY TotalSalesValue DESC;
+
+
+--ما أداء المبيعات  سنوي
+SELECT 
+    d.[Calendar Year],
+    d.[Calendar Year Label],
+    SUM(s.[Total Excluding Tax] + s.[Tax Amount]) AS yearRevenue
+FROM Fact.Sale s
+JOIN Dimension.Date d ON s.[Delivery Date Key] = d.Date
+WHERE d.[Calendar Year] = 2016
+GROUP BY d.[Calendar Year], d.[Calendar Year Label]
+ORDER BY d.[Calendar Year], d.[Calendar Year Label];
+
+
+--ما هي أكثر المنتجات شراءً من حيث عدد الطلبات لكل مورد؟ 
+SELECT
+    DS.[Supplier],
+    DSI.[Stock Item],
+    COUNT(DISTINCT FP.[WWI Purchase Order ID]) AS PurchaseOrderCount
+FROM Fact.Purchase FP
+JOIN Dimension.Supplier DS ON FP.[Supplier Key] = DS.[Supplier Key]
+JOIN Dimension.[Stock Item] DSI ON FP.[Stock Item Key] = DSI.[Stock Item Key]
+GROUP BY DS.[Supplier], DSI.[Stock Item]
+ORDER BY PurchaseOrderCount DESC;
+
+-- ما هو متوسط الكمية المطلوبة يوميًا لكل مورد؟
+
+SELECT
+    DD.[Date],
+    DS.[Supplier],
+    AVG(FP.[Ordered Quantity]) AS AvgDailyOrderedQuantity
+FROM Fact.Purchase FP
+JOIN Dimension.Supplier DS ON FP.[Supplier Key] = DS.[Supplier Key]
+JOIN Dimension.Date DD ON FP.[Date Key] = DD.Date
+GROUP BY DD.[Date], DS.[Supplier]
+ORDER BY AvgDailyOrderedQuantity DESC;
